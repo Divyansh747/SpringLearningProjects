6a6c513deff849c15feaa5cf07371c547f06a7ad
Divyansh747 <divyansh.1999.dr@gmail.com>
2022-09-09 14:22:25 +0530

    Iteration 3: Loose Coupling using Spring
    Here we performed Step 2 of Loose coupling concept using Spring.

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,18 +1,23 @@
 package com.example.demo;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import com.example.demo.game.GameRunner;
-import com.example.demo.game.SuperContraGame;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		// SpringApplication.run(DemoApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+
+		// MarioGame,SuperContraGame Context selector
+		GameRunner runner = context.getBean(GameRunner.class);
+
 		// MarioGame game = new MarioGame();
-		SuperContraGame game = new SuperContraGame();
-		GameRunner runner = new GameRunner(game);
+		// SuperContraGame game = new SuperContraGame();
+		// GameRunner runner = new GameRunner(game);
 		runner.runGame();
 	}
 
