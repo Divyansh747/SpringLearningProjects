92496e88bbf9833991ea27fdb06f3fbcbeb0aec3
Divyansh747 <divyansh.1999.dr@gmail.com>
2022-09-09 16:39:47 +0530

    Added API to display course by ID
    static code to display course by Id

--- learn-spring-boot/src/main/java/com/springexample/learnspringboot/courses/controller/CourseController.java
+++ learn-spring-boot/src/main/java/com/springexample/learnspringboot/courses/controller/CourseController.java
@@ -13,6 +13,12 @@ public class CourseController {
 
 	@GetMapping("/courses")
 	public List<Course> getAllcourses() {
-		return Arrays.asList(new Course(1, "Learn Microservices", "in28Minutes"));
+		return Arrays.asList(new Course(1, "Learn Microservices", "in28Minutes"),
+				new Course(2, "Learn Docker", "in28Minutes"));
+	}
+
+	@GetMapping("/courses/1")
+	public Course getCourseById() {
+		return new Course(1, "Learn Microservices", "in28Minutes");
 	}
 }
