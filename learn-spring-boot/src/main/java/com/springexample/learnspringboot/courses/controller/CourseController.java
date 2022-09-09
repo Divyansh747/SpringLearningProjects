f162a710e8e8e1dd2202c39f67695fc6ffdda33b
Divyansh747 <divyansh.1999.dr@gmail.com>
2022-09-09 17:56:45 +0530

    Added Post Put Delete Apis

--- learn-spring-boot/src/main/java/com/springexample/learnspringboot/courses/controller/CourseController.java
+++ learn-spring-boot/src/main/java/com/springexample/learnspringboot/courses/controller/CourseController.java
@@ -4,8 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springexample.learnspringboot.courses.bean.Course;
@@ -29,5 +33,20 @@ public class CourseController {
 			throw new RuntimeException("Course not found with id " + id);
 		}
 		return course.get();
+	}
+
+	@PostMapping("/courses")
+	public void createCourse(@RequestBody Course course) {
+		courseRepository.save(course);
+	}
+
+	@PutMapping("/courses/{id}")
+	public void updateCourse(@PathVariable long id, @RequestBody Course course) {
+		courseRepository.save(course);
+	}
+
+	@DeleteMapping("/courses/{id}")
+	public void deleteCourse(@PathVariable long id) {
+		courseRepository.deleteById(id);
 	}
 }
