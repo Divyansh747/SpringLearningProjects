357c3c98920711bd16730be2da7d4268a23bd0af
Divyansh747 <divyansh.1999.dr@gmail.com>
2022-09-12 19:04:17 +0530

    added dynamic filter

--- restful-web-services/src/main/java/com/demo/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ restful-web-services/src/main/java/com/demo/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -1,7 +1,12 @@
 package com.demo.rest.webservices.restfulwebservices.filtering;
 
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 public class FilteringController {
@@ -9,5 +14,16 @@ public class FilteringController {
 	@GetMapping("/filtering")
 	public SomeBean filtering() {
 		return new SomeBean("value1", "value2", "value3");
+	}
+
+	@GetMapping("/dynamic-filtering")
+	public MappingJacksonValue filteringDybamic() {
+		SomeBean someBean = new SomeBean("value1", "value2", "value3");
+		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean);
+
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1");
+		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+		mappingJacksonValue.setFilters(filters);
+		return mappingJacksonValue;
 	}
 }
